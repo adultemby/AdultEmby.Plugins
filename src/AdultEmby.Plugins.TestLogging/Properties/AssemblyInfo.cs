2ced13b75143a048dfed49fcdb1be6b8ad7c8aa2
AdultEmby <adultemby@users.noreply.github.com>
2017-12-01 19:00:12 +0000

    Migrated TestLogging plugin to .net standard 1.3

--- src/AdultEmby.Plugins.TestLogging/Properties/AssemblyInfo.cs
+++ src/AdultEmby.Plugins.TestLogging/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdultEmby.Plugins.TestLogging")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("AdultEmby.Plugins.TestLogging")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//[assembly: AssemblyTitle("AdultEmby.Plugins.TestLogging")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyProduct("AdultEmby.Plugins.TestLogging")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b50cfad8-e661-4c6c-96aa-98e734782966")]
+//[assembly: Guid("b50cfad8-e661-4c6c-96aa-98e734782966")]
